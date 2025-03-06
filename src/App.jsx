1d541fd3869be6088ebdabe1de5df63b6c39821d
ez-BYL <limbubardan5@gmail.com>
2025-03-06 22:44:46 +0545

    hello

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// Import React and the TodoList component
+import React from 'react';
+import TodoList from './TodoList';
+
+// Define the App component
+function App() {
+  // Render the TodoList component
+  return <TodoList />;
+}
+
+// Export the App component as the default export
+export default App;
